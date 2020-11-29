1d94ed73263ef7c071df5b44c45509a6703a8b97
fernandomarca <fernandomarca@hotmail.com>
2020-11-29 16:01:41 -0300

    firebase functions

--- quickstarts/big-ben/public/script.js
+++ quickstarts/big-ben/public/script.js
@@ -1,27 +1,12 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 function refresh(_this) {
-  // Disable the button and remove current BONGS text.
+  console.log('aqui');
   _this.disabled = true;
   document.getElementById('bongs').innerHTML = '...';
 
   // Prepare an ajax request to use the API to get the current BONGS from the API.
   var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
+  request.onreadystatechange = function () {
     if (request.readyState === 4) {
       // Re-enable the button.
       _this.disabled = false;
@@ -30,7 +15,7 @@ function refresh(_this) {
         // Replace the BONG text with the response from the API.
         bongsContainer.innerHTML = JSON.parse(request.responseText).bongs;
       } else {
-        bongsContainer.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+        bongsContainer.innerHTML = 'An error occurred during your request: ' + request.status + ' ' + request.statusText;
       }
     }
   }
